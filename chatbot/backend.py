1d2d0a89f5be7cc54b50543ee4ed2f9cc1187993
Ashita Aswal <ashita26102000@gmail.com>
2025-11-05 23:04:08 +0530

    added db connection.

--- chatbot/backend.py
+++ chatbot/backend.py
@@ -2,10 +2,12 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage,HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
 import os
+import sqlite3
+
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",google_api_key=os.getenv("GOOGLE_API_KEY"))
@@ -18,8 +20,12 @@ def chat_node(state: ChatState):
     response = llm.invoke(messages)
     return {"messages": [response]}
 
+
+#CONNECT WITH SQLITE
+conn = sqlite3.connect(database='chatbot.db',check_same_thread=False)
+
 # Checkpointer
-checkpointer = InMemorySaver()
+checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
@@ -27,7 +33,14 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
-CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
+#CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
 # chatbot.stream()
 
--- chatbot/frontend.py
+++ chatbot/frontend.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from backend import chatbot
+from backend import chatbot, retrieve_all_threads
 from langchain_core.messages import HumanMessage
 import uuid
 #******************************************Utilities******************************
@@ -32,7 +32,7 @@ if "thread_id" not in st.session_state:
     st.session_state["thread_id"] = generate_thread_id()
 
 if "all_threads" not in st.session_state:
-    st.session_state["all_threads"] = []
+    st.session_state["all_threads"] = retrieve_all_threads()
 
 add_threads(st.session_state["thread_id"])
 
